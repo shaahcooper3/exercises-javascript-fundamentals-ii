1eba8517d272ae41de9d1523e7c0c399e6f8fc42
Jesse Farmer <jesse@20bits.com>
2020-01-17 14:03:29 -0800

    Add helper to run tests with nicer output

--- exercises/formatSeconds/formatSeconds.js
+++ exercises/formatSeconds/formatSeconds.js
@@ -23,27 +23,31 @@ function formatSeconds(num) {
   // make sure you can do it by hand.
 }
 
+function testFormatSeconds(input, expectedOutput) {
+  let actualOutput = formatSeconds(input);
+  let msg;
+
+  if (expectedOutput === actualOutput) {
+    msg = 'PASS';
+  } else {
+    msg = 'FAIL';
+  }
+
+  console.log('[%s] %s === %s',  msg, expectedOutput, actualOutput);
+}
+
 if (require.main === module) {
   console.log('Running sanity checks for formatSeconds:');
 
-  /*
-  Add your own test cases here! These four aren't enough.
+  testFormatSeconds(0, '0s');
+  testFormatSeconds(1, '1s');
 
-  Notice that we're looking at "edge cases": the boundary where the
-  parts "flip over", and also the values on either side of that boundary.
+  testFormatSeconds(55, '55s');
+  testFormatSeconds(60, '1m 0s');
+  testFormatSeconds(65, '1m 5s');
 
-  This is where the code is most likely to go wrong.
-  */
-
-  console.log(formatSeconds(0) === '0s');
-  console.log(formatSeconds(1) === '1s');
-
-  console.log(formatSeconds(55) === '55s');
-  console.log(formatSeconds(60) === '1m 0s');
-  console.log(formatSeconds(65) === '1m 5s');
-
-  console.log(formatSeconds(3600) === '1h 0m 0s');
-  console.log(formatSeconds(3615) === '1h 0m 15s');
+  testFormatSeconds(3600, '1h 0m 0s');
+  testFormatSeconds(3615, '1h 0m 15s');
 }
 
 module.exports = formatSeconds;
